0855b55272b687679a21336c48bdcfca94c2e0f3
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-10-21 23:32:56 +0000

    automatically backed up by learn

--- app/show.rb
+++ app/show.rb
@@ -1,13 +1,13 @@
 class Show < ActiveRecord::Base
-  def Show::highest_rating
+  def highest_rating
     Show.maximum(:rating)
   end
 
-  def Show::lowest_rating
+  def lowest_rating
     Show.minimum(:rating)
   end
 
-  def Show::ratings_sum
+  def ratings_sum
     Show.sum(:rating)
   end
 
